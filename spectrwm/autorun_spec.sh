f16e18c2fac6063649c3843b5906c3b1fba50e67
MACM <marcoacardosom@gmail.com>
2020-07-12 16:35:45 -0500

    Update autorun_spec.sh

--- spectrwm/autorun_spec.sh
+++ spectrwm/autorun_spec.sh
@@ -1,20 +1,20 @@
 #! /usr/bin/sh
 
 # Wallpaper selection
-# nitrogen --restore &
-wal -R
+nitrogen --restore &
+# wal -R
 
 xrdb -merge .Xresources
 
 # compton to make every non focused windows transparent
-compton -i .9
+# picom -i 0.9
 
 # redshift. Elimina luz azul en la noche
-redshift-gtk &
+# redshift-gtk &
+redshift -P -O 4500
 
 # montar automaticamente medios externos
 udiskie &
 
 # speed up keyboard
-xset r rate 300 50
-# conky -c ~/.config/qtile/scripts/system-overview &
+xset r rate 300 50 &
