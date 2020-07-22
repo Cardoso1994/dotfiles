5727a769aab542f9f2a9f29b0cfa88c6eed0c52e
Marco Cardoso <marcoacardosom@gmail.com>
2020-07-22 18:11:15 -0500

    script to control touchpad

---
+++ i3/scripts/pad_on_off.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+#
+#   Script that enables and disables touchpad on the fly.
+#   Also enables click on tap
+#
+id=`xinput list | grep Touchpad | grep -o id=.. | awk -F'=' '{ print $2 }'`
+
+prop=`xinput list-props $id | grep -m 1 "Tapping Enabled" | awk -F'(' '{ print $2 }' \
+    | awk -F')' '{ print $1 }'`
+
+case $1 in
+    on )
+        xinput set-prop $id "Device Enabled" 1;;
+    off )
+        xinput set-prop $id "Device Enabled" 0;;
+    click )
+        xinput set-prop $id $prop 1;;
+esac
