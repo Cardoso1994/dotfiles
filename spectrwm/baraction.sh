7195cf45aa8092dbbb93307f1483691b26564f11
Marco Cardoso <marcoacardosom@gmail.com>
2020-07-26 16:18:32 -0500

    update 26 - jul - 2020

--- spectrwm/baraction.sh
+++ spectrwm/baraction.sh
@@ -28,8 +28,10 @@ cpu() {
   read cpu a b c idle rest < /proc/stat
   total=$((a+b+c+idle))
   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-  # echo -e "CPU: $cpu%"
-  echo "$cpu%"
+  temp=`sensors | grep temp1: | head -1 | awk -F' ' '{ print $2 }'`
+  temp="${temp:1}"
+
+  echo "$cpu% $temp"
 }
 
 ## VOLUME
@@ -53,7 +55,7 @@ bat () {
 
     bat=`acpi | awk -F',' 'END{ print $2 }' | sed 's/%//'`
 
-    if [ $bat -le 10 ] && [ chr != "Discharging"  ]; then
+    if [ $bat -le 10 ] && [ chr == "Discharging"  ]; then
         notify-send "LOW BATTERY"
     fi
 }
@@ -61,6 +63,6 @@ bat () {
 SLEEP_SEC=1
 #loops forever outputting a line every SLEEP_SEC secs
 while :; do
-    echo "+@bg=1;+@fg=0; +@fn=1;力+@fn=0; $(cpu)  +@bg=0;  +@bg=2;  +@fn=0;+@fn=1; $(mem)  +@bg=0;  +@bg=3; +@fn=1;+@fn=0; $(hdd)  +@bg=0;  +@bg=4; +@fn=1;墳+@fn=0; $(vol)  +@bg=0;  +@bg=5; +@fn=1;+@fn=0;$(bat)  "
+    echo "+@bg=1;+@fg=0; +@fn=1;力+@fn=0; $(cpu) +@bg=0;  +@bg=2;  +@fn=1;+@fn=0; $(mem) +@bg=0;  +@bg=3; +@fn=1;+@fn=0; $(hdd) +@bg=0;  +@bg=4; +@fn=1;墳+@fn=0; $(vol) +@bg=0;  +@bg=5; +@fn=1;+@fn=0;$(bat) "
     sleep $SLEEP_SEC
 done
