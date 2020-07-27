a5367c0181f7ff649b137df845c753ae98b6b9e8
Marco Cardoso <marcoacardosom@gmail.com>
2020-07-26 21:55:14 -0500

    added Mac touchpad

--- scripts/pad_on_off.sh
+++ scripts/pad_on_off.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
 
+############################################################
 #
 #   Script that enables and disables touchpad on the fly.
 #   Also enables click on tap
 #
-id=`xinput list | grep Touchpad | grep -o id=.. | awk -F'=' '{ print $2 }'`
+############################################################
+
+# Asus
+# id=`xinput list | grep Touchpad | grep -o id=.. | awk -F'=' '{ print $2 }'`
+# Mac
+id=`xinput list | grep "bcm5" | grep -o id=.. | awk -F'=' '{ print $2 }'`
 
 prop=`xinput list-props $id | grep -m 1 "Tapping Enabled" | awk -F'(' '{ print $2 }' \
     | awk -F')' '{ print $1 }'`
