1d59d37442b91525a63af5bc33c72c3f4d255de9
Marco Cardoso <marcoacardosom@gmail.com>
2020-08-21 21:44:07 -0500

    added arch update script for i3blocks, and minor update in other scripts

--- scripts/pad_on_off.sh
+++ scripts/pad_on_off.sh
@@ -8,12 +8,11 @@
 ############################################################
 
 # Asus
-id=`xinput list | grep Touchpad | grep -o id=.. | awk -F'=' '{ print $2 }'`
+# id=`xinput list | grep Touchpad | grep -o id=.. | awk -F'=' '{ print $2 }'`
 # Mac
-# id=`xinput list | grep "bcm5" | grep -o id=.. | awk -F'=' '{ print $2 }'`
+id=`xinput list | grep "bcm5" | grep -o id=.. | awk -F'=' '{ print $2 }'`
 
 prop=`xinput list-props $id | grep -m 1 "Tapping Enabled" | awk -F'(' '{ print $2 }' \
-    | awk -F')' '{ print $1 }'`
 
 case $1 in
     on )
